23e93fb9d3864eee82fc7f7e9bad6d3d1f1f1704
pilarskimarcin <62024556+pilarskimarcin@users.noreply.github.com>
2024-09-10 19:04:45 +0200

    Obsługa scenariusza i testy

--- Skrypty/scenario_classes.py
+++ Skrypty/scenario_classes.py
@@ -5,9 +5,11 @@ from typing import Dict, List, Tuple
 from Skrypty.sor_classes import Hospital, Department
 from Skrypty.utilities import PlaceAddress
 from Skrypty.victim_classes import Victim
-from Skrypty.zrm_classes import ZRM
+from Skrypty.zrm_classes import ZRM, ZRMType
 
 DATA_TITLE_OFFSET: int = 1
+ZRM_DATA_TITLE: str = "ZRM: "
+ADDRESS_DATA_TITLE: str = "Adres: "
 
 
 class Scenario:
@@ -35,42 +37,79 @@ class Scenario:
         departments_data: List[str] = departments_string.split("\n")[DATA_TITLE_OFFSET:]
         SOR_table: pd.DataFrame = pd.read_csv("../Dane/SOR.csv", encoding="utf-8", sep=";", index_col=0)
         self.hospitals = []
-        hospitals_and_departments: Dict[Tuple[str, str], List[Department]] = self.JoinDepartmentDataWithHospitals(
-            departments_data, SOR_table
-        )
+        hospitals_and_departments: Dict[Tuple[str, str], List[Department]] = self.AddDepartmentsToHospitals(
+            departments_data, SOR_table)
         hospital_id: int = 1
         for key, departments in hospitals_and_departments.items():
             hospital_name, address_str = key
-            address_parts: List[str] = address_str.split()
-            address: PlaceAddress = PlaceAddress(street=address_parts[0], number=int(address_parts[1]),
-                                                 postal_code=address_parts[2], city=address_parts[3])
+            address: PlaceAddress = PlaceAddress.FromString(address_str)
             self.hospitals.append(
                 Hospital(id_=hospital_id, name=hospital_name, address=address, departments=departments)
             )
             hospital_id += 1
 
     @staticmethod
-    def JoinDepartmentDataWithHospitals(
-            departments_data: List[str], hospital_data_table: pd.DataFrame
-    ) -> Dict[Tuple[str, str], List[Department]]:
+    def AddDepartmentsToHospitals(departments_data: List[str], hospital_data_table: pd.DataFrame) -> (
+            Dict)[Tuple[str, str], List[Department]]:
         departments: Dict[Tuple[str, str], List[Department]] = dict()
         for department_data in departments_data:
             department_number, beds_number = [int(number) for number in department_data.split()]
             department_row: pd.Series = hospital_data_table.loc[department_number]
-            _, hospital_name, address, department_name, medical_disciplines, _ = department_row.tolist()
+            _, hospital_name, address, department_name, medical_disciplines_string, _ = department_row.tolist()
             if (hospital_name, address) not in departments.keys():
                 departments[hospital_name, address] = []
+            medical_disciplines: List[int] = Scenario.GetMedicalDisciplinesFromString(medical_disciplines_string)
             departments[hospital_name, address].append(
                 Department(id_=department_number, name=department_name, medical_categories=medical_disciplines,
                            current_beds_count=beds_number)
             )
         return departments
 
-    def ParseTeams(self, teams_part):
-        pass
+    @staticmethod
+    def GetMedicalDisciplinesFromString(medical_disciplines_string: str) -> List[int]:
+        disciplines_string_list: List[str] = medical_disciplines_string.split(", ")
+        return [int(discipline_string) for discipline_string in disciplines_string_list]
 
-    def ParseVictims(self, victims_part, total_victims_part):
-        pass
+    def ParseTeams(self, teams_part: str):
+        teams_part_without_title: str = teams_part[len(ZRM_DATA_TITLE):]
+        teams_ids: List[int] = [int(number) for number in teams_part_without_title.split(", ")]
+        self.GetTeamsDataFromIds(teams_ids)
 
-    def ParseAddress(self, address_part):
-        pass
+    def GetTeamsDataFromIds(self, teams_ids: List[int]):
+        full_teams_data: pd.DataFrame = pd.read_csv("../Dane/ZRM.csv", encoding="utf-8", sep=";", index_col=0)
+        self.teams = []
+        for team_id in teams_ids:
+            team_row: pd.Series = full_teams_data.loc[team_id]
+            team_number, address, dispatch, team_type = team_row.tolist()
+            zrm_parsed_type: ZRMType = eval(f"ZRMType.{team_type}", globals(), locals())
+            self.teams.append(ZRM(team_number, dispatch, zrm_parsed_type, PlaceAddress.FromString(address)))
+
+    def ParseVictims(self, victims_part: str, total_victims_part: str):
+        profiles_and_counts: List[Tuple[str, int]] = []
+        sum_victims_counts: int = 0
+        for line in victims_part.split("\n")[DATA_TITLE_OFFSET:]:
+            profile, amount_string = line.split()
+            profiles_and_counts.append((profile, int(amount_string)))
+            sum_victims_counts += int(amount_string)
+        total_victims_count: int = int(total_victims_part.split()[-1])
+        if total_victims_count != sum_victims_counts:
+            raise RuntimeError(f"Całkowita liczba poszkodowanych {total_victims_count} nie jest równa sumie liczb "
+                               f"pposzkodowanych {sum_victims_counts}")
+        self.CreateVictimsFromProfilesAndCounts(profiles_and_counts)
+
+    def CreateVictimsFromProfilesAndCounts(self, profiles_and_counts: List[Tuple[str, int]]):
+        self.victims = []
+        victim_id: int = 1
+        for profile, count in profiles_and_counts:
+            path: str = "../Profile pacjentów/" + profile + ".txt"
+            with open(path, encoding="utf-8") as f:
+                profile_text: str = f.read()
+                for _ in range(count):
+                    self.victims.append(
+                        Victim.FromString(profile_text, victim_id)
+                    )
+                    victim_id += 1
+
+    def ParseAddress(self, address_part: str):
+        address_part_without_title: str = address_part[len(ADDRESS_DATA_TITLE):]
+        self.address = PlaceAddress.FromString(address_part_without_title.replace(",", ""))
--- Testy/test_scenario_classes.py
+++ Testy/test_scenario_classes.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from typing import List, Tuple
+import pandas as pd
+from typing import Dict, List, Tuple
 import unittest
 
 from Skrypty import scenario_classes as scenario
@@ -7,87 +8,139 @@ from Skrypty import sor_classes as sor
 from Skrypty import utilities as util
 from Skrypty import victim_classes as victim
 from Skrypty import zrm_classes as zrm
+from Testy import tests_sor_classes as tests_sor
 
 
 def CreateSampleScenarioData() -> Tuple[sor.Hospital, list[zrm.ZRM], list[victim.Victim], util.PlaceAddress]:
+    sample_hospital: sor.Hospital = CreateSampleHospital()
+    sample_teams: List[zrm.ZRM] = CreateSampleTeams()
+    sample_victims: List[victim.Victim] = tests_sor.CreateSampleVictims()
+    sample_address2: util.PlaceAddress = tests_sor.CreateSampleIncidentPlaceAddress()
+    return sample_hospital, sample_teams, sample_victims, sample_address2
+
+
+def CreateSampleHospital() -> sor.Hospital:
     sample_departments: List[sor.Department] = [
         sor.Department(id_=1, name="Oddział chirurgii urazowo-ortopedycznej", medical_categories=[25],
-                       current_beds_count=22),
+                       current_beds_count=10),
         sor.Department(id_=2, name="Oddział kardiologiczny", medical_categories=[53],
-                       current_beds_count=31),
+                       current_beds_count=14),
         sor.Department(id_=3, name="Oddział anestezjologii i intensywnej terapii", medical_categories=[1],
-                       current_beds_count=9),
+                       current_beds_count=2),
         sor.Department(id_=4, name="Oddział chirurgii naczyniowej", medical_categories=[39, 37],
-                       current_beds_count=15),
+                       current_beds_count=5),
         sor.Department(id_=5, name="Oddział neurologiczny z pododziałem udarowym", medical_categories=[22],
-                       current_beds_count=15),
+                       current_beds_count=5),
         sor.Department(id_=6, name="Szpitalny Oddział Ratunkowy", medical_categories=[15],
-                       current_beds_count=12),
+                       current_beds_count=6),
         sor.Department(id_=7, name="Pododdział udarowy", medical_categories=[22],
-                       current_beds_count=16)
+                       current_beds_count=5)
     ]
     sample_address: util.PlaceAddress = util.PlaceAddress("Topolowa", 16, "32-500", "Chrzanów")
-    sample_hospital: sor.Hospital = sor.Hospital(
+    return sor.Hospital(
         id_=1, name="Szpital Powiatowy w Chrzanowie",
         address=sample_address,
         departments=sample_departments
     )
-    sample_teams: List[zrm.ZRM] = [
+
+
+def CreateSampleTeams() -> List[zrm.ZRM]:
+    sample_address: util.PlaceAddress = util.PlaceAddress("Topolowa", 16, "32-500", "Chrzanów")
+    return [
         zrm.ZRM(id_="K01 47", dispatch="DM06-01", zrm_type=zrm.ZRMType.S, base_location=sample_address),
         zrm.ZRM(id_="K01 098", dispatch="DM06-01", zrm_type=zrm.ZRMType.P, base_location=sample_address)
     ]
-    sample_state1: victim.State = victim.State(
-        number=1, is_victim_walking=False, respiratory_rate=12, pulse_rate=120, is_victim_following_orders=True,
-        triage_colour=victim.TriageColour.YELLOW,
-        health_problems_ids=(
-            victim.HealthProblem(25, 1), victim.HealthProblem(25, 4)
-        ),
-        description=u"Kobieta lat 17 zgłasza problem z poruszaniem się, na prośbę o opuszczenie strefy "
-                    u"niebezpiecznej, zgłasza ból w obrębie stawu kolanowego, dolegliwości uniemożliwiają "
-                    u"samodzielne poruszanie się, dolegliwości bólowe w obrębie stawu barkowego (widoczna "
-                    u"asymetria barku lewego w stosunku do prawego, bolesność palpacyjna)."
-    )
-    sample_state2: victim.State = victim.State(
-        number=1, is_victim_walking=False, respiratory_rate=12, pulse_rate=130, is_victim_following_orders=True,
-        triage_colour=victim.TriageColour.YELLOW,
-        health_problems_ids=(
-            victim.HealthProblem(25, 2)
-        ),
-        description=u"Mężczyzna lat 13 zgłasza problem z poruszaniem się, na prośbę o opuszczenie strefy "
-                    u"niebezpiecznej, zgłasza ból w obrębie piszczeli prawej, widoczna deformacja, widoczny uraz "
-                    u"głowy nie wymagający interwencji chirurgicznej."
-    )
-    sample_victims: List[victim.Victim] = [
-        victim.Victim(1, tuple([sample_state1])), victim.Victim(2, tuple([sample_state1])),
-        victim.Victim(3, tuple([sample_state2])), victim.Victim(4, tuple([sample_state2])),
-    ]
-    sample_address2: util.PlaceAddress = util.PlaceAddress("Magnoliowa", 10, "32-500", "Chrzanów")
-    return sample_hospital, sample_teams, sample_victims, sample_address2
 
 
 class TestScenario(unittest.TestCase):
+    sample_scenario: scenario.Scenario
 
     def setUp(self):
         self.sample_scenario = scenario.Scenario("../Scenariusze/Scenariusz 1.txt")
 
     def testInit(self):
         sample_hospital, sample_teams, sample_victims, sample_address = CreateSampleScenarioData()
+
         self.assertEqual(self.sample_scenario.hospitals, [sample_hospital])
         self.assertEqual(self.sample_scenario.teams, sample_teams)
         self.assertEqual(self.sample_scenario.victims, sample_victims)
         self.assertEqual(self.sample_scenario.address, sample_address)
 
     def testParseDepartments(self):
-        raise NotImplementedError
+        sample_input: str = "Oddziały - liczba miejsc\n1 10\n2 14\n3 2\n4 5\n5 5\n6 6\n7 5"
+        sample_hospital: sor.Hospital = CreateSampleHospital()
+
+        self.sample_scenario.ParseDepartments(sample_input)
+
+        self.assertEqual(self.sample_scenario.hospitals, [sample_hospital])
+
+    def testAddDepartmentsToHospitals(self):
+        sample_departments_data: List[str] = ["1 10", "2 14", "3 2", "4 5", "5 5", "6 6", "7 5"]
+        SOR_table: pd.DataFrame = pd.read_csv("../Dane/SOR.csv", encoding="utf-8", sep=";", index_col=0)
+        hospitals_and_departments_dict: Dict[Tuple[str, str], List[sor.Department]] = (
+            scenario.Scenario.AddDepartmentsToHospitals(sample_departments_data, SOR_table)
+        )
+        sample_hospital: sor.Hospital = CreateSampleHospital()
+        sample_hospital_with_joined_departments: Dict[Tuple[str, str], List[sor.Department]] = {
+            (sample_hospital.name, sample_hospital.address.address_for_places_data): sample_hospital.departments
+        }
+
+        self.assertEqual(hospitals_and_departments_dict, sample_hospital_with_joined_departments)
+
+    def testGetMedicalDisciplinesFromStringMoreDisciplines(self):
+        sample_disciplines_string: str = "25, 14"
+        parsed_disciplines: List[int] = scenario.Scenario.GetMedicalDisciplinesFromString(sample_disciplines_string)
+
+        self.assertEqual(parsed_disciplines, [25, 14])
+
+    def testGetMedicalDisciplinesFromStringOneDiscipline(self):
+        sample_disciplines_string: str = "25"
+        parsed_disciplines: List[int] = scenario.Scenario.GetMedicalDisciplinesFromString(sample_disciplines_string)
+
+        self.assertEqual(parsed_disciplines, [25])
 
     def testParseTeams(self):
-        raise NotImplementedError
+        sample_input: str = "ZRM: 1, 2"
+        sample_teams: List[zrm.ZRM] = CreateSampleTeams()
+        self.sample_scenario.ParseTeams(sample_input)
 
-    def testParseVictims(self):
-        raise NotImplementedError
+        self.assertEqual(self.sample_scenario.teams, sample_teams)
+
+    def testGetTeamsDataFromIds(self):
+        sample_teams_ids: List[int] = [1, 2]
+        sample_teams: List[zrm.ZRM] = CreateSampleTeams()
+        self.sample_scenario.GetTeamsDataFromIds(sample_teams_ids)
+
+        self.assertEqual(self.sample_scenario.teams, sample_teams)
+
+    def testParseVictimsCorrect(self):
+        sample_input1: str = "Profile - liczba poszkodowanych\nŻółty/Profil70 2\nŻółty/Profil95 2"
+        sample_input2: str = "Całkowita liczba poszkodowanych: 4"
+        sample_victims: List[victim.Victim] = tests_sor.CreateSampleVictims()
+        self.sample_scenario.ParseVictims(sample_input1, sample_input2)
+
+        self.assertEqual(self.sample_scenario.victims, sample_victims)
+
+    def testParseVictimsError(self):
+        sample_input1: str = "Profile - liczba poszkodowanych\nŻółty/Profil70 2\nŻółty/Profil95 2"
+        sample_input2: str = "Całkowita liczba poszkodowanych: 5"
+        self.assertRaises(RuntimeError, self.sample_scenario.ParseVictims, sample_input1, sample_input2)
+
+    def testCreateVictimsFromProfilesAndCounts(self):
+        sample_profiles_and_counts: List[Tuple[str, int]] = [
+            ("Żółty/Profil70", 2), ("Żółty/Profil95", 2)
+        ]
+        sample_victims: List[victim.Victim] = tests_sor.CreateSampleVictims()
+        self.sample_scenario.CreateVictimsFromProfilesAndCounts(sample_profiles_and_counts)
+
+        self.assertEqual(self.sample_scenario.victims, sample_victims)
 
     def testParseAddress(self):
-        raise NotImplementedError
+        sample_input: str = "Adres: Magnoliowa 10, 32-500 Chrzanów"
+        sample_address: util.PlaceAddress = tests_sor.CreateSampleIncidentPlaceAddress()
+        self.sample_scenario.ParseAddress(sample_input)
+
+        self.assertEqual(self.sample_scenario.address, sample_address)
 
 
 if __name__ == '__main__':
