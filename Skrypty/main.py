34a8ea33608948f47228383a462d2d9b2d93962b
pilarskimarcin <62024556+pilarskimarcin@users.noreply.github.com>
2024-10-25 20:00:54 +0200

    Poprawki w działaniu aplikacji

--- Skrypty/main.py
+++ Skrypty/main.py
@@ -1,29 +1,24 @@
 # -*- coding: utf-8 -*-
-import sys
-import traceback
-from typing import List
 import PyQt6.QtCore as Qc
-import PyQt6.QtGui as Qg
 import PyQt6.QtWidgets as Qt
+import datetime
+from typing import List
 
-from Skrypty import scenario_editor as sc_edit
-from Skrypty import simulation as sim
+import scenario_editor as sc_edit
+import simulation as sim
 
 # Parametry dla Qt
 FONT_SIZE: int = 8
 MIN_HEIGHT: int = 450
 MIN_WIDTH: int = 550
+MESSAGE_BOX_MIN_WIDTH: int = 400
+MESSAGE_BOX_MIN_HEIGHT: int = 250
 
 # Inne stałe
 DEAD_COUNT_TEXT: str = "Liczba zmarłych poszkodowanych"
 AVERAGE_RPM_TEXT: str = "Średnia ocena RPM poszkodowanych"
 TOTAL_SIM_TIME_TEXT: str = "Całkowity czas symulacji [min]"
 AVERAGE_HELP_TIME_TEXT: str = "Średni czas pomocy poszkodowanemu [min]"
-
-# Włączenie Qt
-app = Qt.QApplication(sys.argv)
-sys.excepthook = lambda *args: (traceback.print_exception(args[0], args[1], args[2], file=sys.stdout))
-app.setFont(Qg.QFont("Arial", FONT_SIZE))
 
 
 class MainApp(Qt.QMainWindow):
@@ -75,8 +70,8 @@ class MainApp(Qt.QMainWindow):
 
     def Simulate(self):
         simulation: sim.Simulation = sim.Simulation(self.current_scenario)
-        simulation_results: sim.SimulationResults = simulation.PerformSimulation()
-        results_window: Qt.QMessageBox = Qt.QMessageBox()
+        simulation_results: sim.SimulationResultsTuple = simulation.PerformSimulation()
+        results_window: MessageBoxWithScrollArea = MessageBoxWithScrollArea()
         results_window.setWindowTitle("Wyniki symulacji")
         weights = (
             self.dead_count_weight_spinbox.value(), self.average_RPM_weight_spinbox.value(),
@@ -96,7 +91,17 @@ class MainApp(Qt.QMainWindow):
         )
         message_lines.append(f"Obliczona wartość funkcji celu: {objective_function_value:.2f}")
         results_window.setText("\n".join(message_lines))
+        solution_as_strings: List[str] = ["\n\nRozwiązanie"] + [str(item) for item in simulation.solution]
+        results_window.scroll_area_content.setText("\n".join(solution_as_strings))
         results_window.exec()
+        file_name_format: str = '%Y%m%d-%H%M%S'
+        scenario_name: str = self.current_scenario.split('/')[-1].split('.')[0]
+        with open(
+                f"../Wyniki/{scenario_name} - wyniki {datetime.datetime.now().strftime(file_name_format)}.txt",
+                "w", encoding="utf-8"
+        ) as f:
+            f.write("\n".join(message_lines))
+            f.write("\n".join(solution_as_strings))
 
     def Layout(self):
         self.main_layout = Qt.QVBoxLayout()
@@ -168,7 +173,21 @@ class MainApp(Qt.QMainWindow):
         layout.addWidget(weight_spinbox)
 
 
+class MessageBoxWithScrollArea(Qt.QMessageBox):
+    def __init__(self, *args, **kwargs):
+        Qt.QMessageBox.__init__(self, *args, **kwargs)
+        self.setTextInteractionFlags(Qc.Qt.TextInteractionFlag.TextSelectableByMouse)
+        scroll_area = Qt.QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setMinimumSize(MESSAGE_BOX_MIN_WIDTH, MESSAGE_BOX_MIN_HEIGHT)
+        self.scroll_area_content: Qt.QLabel = Qt.QLabel()
+        self.scroll_area_content.setWordWrap(True)
+        self.scroll_area_content.setTextInteractionFlags(Qc.Qt.TextInteractionFlag.TextSelectableByMouse)
+        scroll_area.setWidget(self.scroll_area_content)
+        # noinspection PyTypeChecker
+        grid_layout: Qt.QGridLayout = self.layout()
+        grid_layout.addWidget(scroll_area, grid_layout.rowCount(), 0, -1, -1)
+
+
 if __name__ == '__main__':
-    window = MainApp()
-    window.show()
-    app.exec()
+    sc_edit.RunGUIApp(MainApp)
